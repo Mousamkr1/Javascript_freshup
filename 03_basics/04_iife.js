35875a0af4ea0da6158ee1ad4eed5efec7c0b53e
Mousamkr1 <mkb1606@gmail.com>
2025-04-25 23:21:19 +0530

    iife

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,13 @@
+// to excape from global scope polution we use/  declare iife
+
+// Immediately Invoked Function Expressions (IIFE)
+
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('hitesh')
